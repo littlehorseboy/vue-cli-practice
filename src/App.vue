2a104626f3738b522b97f7d5490998c7aa384fba
unknown <littlehorseboy@geosat.com.tw>
2018-03-23 16:42:44 +0800

    vue-cli-cross-domain

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+
+export default {
+  name: 'App',
+  created() {
+    Vue.axios.get('/data/youbike')
+      .then((response) => {
+        console.log(response.data);
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
